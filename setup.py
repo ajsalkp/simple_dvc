c1b8bc62a2df2264fec74cc19f1785d900ab366a
ajsalkp <kpajsal@gmail.com>
2021-05-11 20:35:22 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='src',
+    version="0.0.1",
+    description="it is wine package",
+    author='ajsal',
+    packages=find_packages(),
+    license='MIT'
+)
